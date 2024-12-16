742dc7764666c172a922fcc68a84cfd2985e1f87
pleump9 <pleum.p9@gmail.com>
2024-12-17 05:57:28 +0700

    [Update] pokemon get.

--- server/main.js
+++ server/main.js
@@ -10,6 +10,7 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+// List Pokemon
 app.get("/pokemon/list", async (req, res) => {
   const limit = req.query.limit ?? 10;
   const offset = req.query.offset ?? 0;
@@ -25,6 +26,27 @@ app.get("/pokemon/list", async (req, res) => {
       statusText: response.statusText,
       totalCount: response.data.count,
       data: response.data.results,
+    });
+  } catch (error) {
+    res.status(error.response?.status ?? 500).json({
+      success: false,
+      message: error.response?.data?.message ?? "An error occurred",
+    });
+  }
+});
+
+// Get Pokemon
+app.get("/pokemon/:nameOrId", async (req, res) => {
+  const { nameOrId } = req.params;
+
+  try {
+    const response = await axios.get(`${pokemonApi}/${nameOrId}`);
+
+    res.json({
+      success: true,
+      status: response.status,
+      statusText: response.statusText,
+      data: response.data,
     });
   } catch (error) {
     res.status(error.response?.status ?? 500).json({
