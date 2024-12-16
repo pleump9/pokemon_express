2027d51d674be5636b8219fb6bca89168ab49497
pleump9 <pleum.p9@gmail.com>
2024-12-17 05:51:12 +0700

    [Update] pokemon list.

--- server/main.js
+++ server/main.js
@@ -1,9 +1,37 @@
 const express = require("express");
+const axios = require("axios");
+
 const app = express();
 const port = 3000;
 
+const pokemonApi = "https://pokeapi.co/api/v2/pokemon1";
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/pokemon-list", async (req, res) => {
+  const limit = req.query.limit ?? 10;
+  const offset = req.query.offset ?? 0;
+
+  try {
+    const response = await axios.get(pokemonApi, {
+      params: { limit, offset },
+    });
+
+    res.json({
+      success: true,
+      status: response.status,
+      statusText: response.statusText,
+      totalCount: response.data.count,
+      data: response.data.results,
+    });
+  } catch (error) {
+    res.status(error.response?.status ?? 500).json({
+      success: false,
+      message: error.response?.data?.message ?? "An error occurred",
+    });
+  }
 });
 
 app.listen(port, () => {
